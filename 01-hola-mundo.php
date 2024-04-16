5b71da5b0115fadc9b3b8ba36fa8dd18ac39ddcc
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-04-16 17:37:50 -0500

    Fundamentos de PhP

---
+++ 01-hola-mundo.php
@@ -0,0 +1,13 @@
+<?php include 'includes/header.php';
+
+print "Hola Mundo";
+
+print ("Hola mundo");
+
+echo ("Hola Mundo");
+
+echo "hOLA MUNDO ";
+
+var_dump (45);
+
+include 'includes/footer.php';
