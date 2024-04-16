62bd371b2cfe05ddb28d3863dfb7cca1ed4ed405
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-04-16 17:56:24 -0500

    Finalización-de-php-tipos-de-datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,5 +1,32 @@
 <?php include 'includes/header.php';
 
+//Boolean
+$logueado = false;
+$logueado = true;
+var_dump($logueado);
+//Tipo de dato entero
+
+$numero = 300;
+var_dump($numero);
+
+
+//Floats
+
+$float = 300.5;
+var_dump($float);
+
+
+//Tipo de dato Strings
+
+$nombre = "Juan";
+var_dump($nombre);
+
+//arreglos
+
+$array = [];
+
+var_dump($array);
+
 
 
 
