2e97e3e1c85793f7b4b42e682c2156e1492fd8e7
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-04-16 18:15:08 -0500

    Operadores de compración en php

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,40 @@
 <?php include 'includes/header.php';
 
 
+$numero = 20;
+$numero1 = 24;
+//SUMAR
+echo "Calculadora de 2 números";
+echo "<br>";
+echo "Número 1:", $numero;
+echo "<br>";
+echo "Número 2:" , $numero1;
+echo "<br>";
+echo "<br>";
+echo "===================================================";
+echo "<br>";
+echo "La suma es:",($numero + $numero1);
+
+echo "<br>";
+//RESTA
+echo "===================================================";
+echo "<br>";
+echo "La resta es: ", ($numero - $numero1);
+
+echo "<br>";
+
+//Multiplicación 
+echo "===================================================";
+echo "<br>";
+echo "La multiplicación es: " ,($numero * $numero1);
+echo "<br>";
+
+echo "===================================================";
+echo "<br>";
+//Division
+echo "La división es: ", ($numero / $numero1);
+
+echo "<br>";
 
 
 include 'includes/footer.php';
--- 05-comparacion.php
+++ 05-comparacion.php
@@ -1,7 +1,43 @@
 <?php include 'includes/header.php';
 
 
+$numero1 = 20;
+$numero2 = 40;
+$numero3 = 40;
+$numero4 = "40";
+
+var_dump($numero1 > $numero2);
+
+echo "<br>";
+
+var_dump($numero1 < $numero2);
+
+echo"<br>";
+
+var_dump($numero1 <= $numero2);
+
+echo "<br>";
+
+var_dump($numero2 >= $numero3);
+
+echo "<br>";
+
+var_dump($numero3 == $numero4);
+
+echo"<br>";
 
 
+//SI en la izquierda es menor = a -1
+var_dump($numero1 <=> $numero2);
+
+echo "<br>";   
+
+//si en la izquierda es mayor = a 1
+var_dump($numero2 <=> $numero1);
+
+echo "<br>";    
+
+// si son iguales entonces es 0 la respuesta;
+var_dump($numero2 <=> $numero3);
 
 include 'includes/footer.php';
