047f179789bc9b2a0207d8b28a4dc1b7afd2cc1c
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-04-16 18:58:53 -0500

    Contador en desenso

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,32 @@
 <?php include 'includes/header.php';
 
 
+$numero1 = 40;
+$numero1++;
+
+echo $numero1;
+
+
+// SI LE PONES ECHO ANTES SE IMPRIME 40
+
+$numero2 = 40;
+echo $numero2;
+
+$numero2++;
+
+//si le imprimers en el mismo echo;
+echo $numero3 = 42;
+
+echo $numero3++;
+echo $numero3;
+
+//decrementos en las variables 
+
+$numero4 = 45;
+
+$numero4++;
+
+echo $numero4;
 
 
 include 'includes/footer.php';
